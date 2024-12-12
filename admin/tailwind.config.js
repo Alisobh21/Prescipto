3a37b8b699b5e23be0a5d0270bcaaf0cd178bee8
Alisobh21 <alisobh21@gmail.com>
2024-12-12 14:23:45 +0300

    Update tailwind.config.js

--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -1,19 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-
-const colors = require('tailwindcss/colors')
-
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    colors:{
-      ...colors,
-      "primary":"#5F6FFF",
-
-    }
+    extend: {
+      colors:{
+        'primary':"#5f6fff"
+      }
+      ,gridTemplateColumns:{
+        "auto":'repeat(auto-fill,minmax(200px,1fr))'
+      }
+    },
   },
   plugins: [],
 }
